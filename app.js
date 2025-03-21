7bb4fc42681408123946b878de169cac8d14fcf8
Gaurav Singh <gauravsingh@MacBookPro.lan>
2025-03-21 12:24:57 +0530

    Code for momwent library usage

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import ls from "lodash";
+import moment from "moment";
 
+console.log("********** lodash **********\n");
 const arr = [1, [2, [3, [4]], 5]];
 
 const arrFlat = ls.flatten(arr); // [1, 2, [3, [4]], 5]
@@ -34,3 +36,69 @@ console.log({ age });
 
 const diff = difference([2, 1], [2, 3]);
 console.log({ diff });
+
+console.log("\n\n********** moment **********\n");
+const now = moment();
+console.log({ now });
+
+const date = moment("2021-01-01");
+console.log({ date });
+
+const date2 = moment("2021-01-01", "YYYY-MM-DD");
+console.log({ date2 });
+
+const date3 = moment("2021-01-01", "YYYY-MM-DD", true);
+console.log({ date3 });
+
+const date4 = moment("2021-01-01", "YYYY-MM-DD", true).format("DD/MM/YYYY");
+console.log({ date4 });
+
+const date5 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss"
+);
+console.log({ date5 });
+
+const date6 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss A"
+);
+console.log({ date6 });
+
+const date7 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss a"
+);
+console.log({ date7 });
+
+const date8 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss a Z"
+);
+console.log({ date8 });
+
+const date9 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss a ZZ"
+);
+console.log({ date9 });
+
+const date10 = moment("2021-01-01", "YYYY-MM-DD", true).format(
+  "DD/MM/YYYY hh:mm:ss a ZZZ"
+);
+console.log({ date10 });
+
+// find date comparison
+
+const date11 = moment("2021-11-01", "YYYY-MM-DD", true);
+const date12 = moment("2021-05-01", "YYYY-MM-DD", true);
+
+console.log(date11.isSame(date12));
+console.log(date11.isSame(date12, "month"));
+
+console.log(date11.isBefore(date12));
+console.log(date11.isAfter(date12));
+
+console.log(date11.isSameOrBefore(date12));
+console.log(date11.isSameOrAfter(date12));
+
+console.log(date11.isBetween("2020-01-01", "2022-01-01"));
+
+console.log(date11.diff(date12));
+console.log(date11.diff(date12, "days"));
+console.log(date11.diff(date12, "months"));
